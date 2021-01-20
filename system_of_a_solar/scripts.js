ee3c7574e2902cf373b92ff9821c9fa167549f44
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-01-20 22:30:13 +0100

    did some stuff, like orbits

--- system_of_a_solar/scripts.js
+++ system_of_a_solar/scripts.js
@@ -1,6 +1,43 @@
 let scene, free_camera;
 let debug_mode = true;
 
+function orbit(pivot, object, angle_x, angle_y, angle_z) {
+	let R = Math.pow((object.position.x - pivot.position.x), 2) +
+		Math.pow((object.position.y - pivot.position.y), 2) +
+		Math.pow((object.position.z - pivot.position.z), 2);
+
+	R = Math.sqrt(R);
+console.log(R);
+
+	let to_return = new THREE.Vector3(
+		pivot.position.x,
+		pivot.position.y + R * Math.sin(Math.PI * angle_x / 180),
+		pivot.position.z + R * Math.cos(Math.PI * angle_x / 180)
+		
+		);
+
+
+	return to_return;
+
+
+}
+
+function moonorbit(pivot, angle_x, R) {
+
+	console.log(R);
+
+	let to_return = new THREE.Vector3(
+		50,
+		pivot.position.y + R * Math.sin(Math.PI * angle_x / 180),
+		pivot.position.z + R * Math.cos(Math.PI * angle_x / 180)
+		
+		);
+
+
+	return to_return;
+
+
+}
 
 
 
@@ -16,6 +53,10 @@ let decay = 0.1;
 let player_obj_geometry, player_obj_material;
 
 let sun, Arrakis, ziemia, vulkan, donut, weird_thing;
+let pivot_Arrakis, pivot_ziemia, pivot_vulkan, pivot_donut, pivot_weird_thing,pivot_moon;
+
+
+
 let cherry, moon;
 
 let ancor_point;
@@ -32,12 +73,12 @@ function init() {
 
 	free_camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-	free_camera.position.z = 10;
+	free_camera.position.z = 100;
 
 
 
 
-	scene.background = new THREE.CubeTextureLoader().setPath('/teemtrees/system_of_a_solar/textures/skybox/').load([
+	scene.background = new THREE.CubeTextureLoader().setPath('/graphic/system_of_a_solar/textures/skybox/').load([
 		'XP.png',
 		'XN.png',
 		'YP.png',
@@ -80,24 +121,34 @@ function init() {
 	scene.add(Arrakis);
 	Arrakis.position.set(30, 45, 0);
 
+
+	pivot_Arrakis = new THREE.Object3D();
+	pivot_Arrakis.position.set(30, 0, 0);
+
+
+
 	// ziemia
 
 	ziemia = new THREE.Mesh(
-		new THREE.SphereGeometry(10, 20, 20, 20),
+		new THREE.CylinderGeometry(10, 10, 1, 30 ),
 		new THREE.MeshBasicMaterial({ color: 0xff3030 }));
 	scene.add(ziemia);
 	ziemia.position.set(60, 90, 0);
+	ziemia.rotation.z+=Math.PI*90/180;
 
-
+	
+	pivot_ziemia = new THREE.Object3D();
+	pivot_ziemia.position.set(60, 0, 0);
 
 	moon = new THREE.Mesh(
-		new THREE.SphereGeometry(10, 20, 20, 20),
-		new THREE.MeshBasicMaterial({ color: 0xff3030 }));
+		new THREE.SphereGeometry(2, 20, 20, 20),
+		new THREE.MeshBasicMaterial({ color: 0x0000ff }));
 	scene.add(moon);
-	moon.position.set(90, 90, 0);
-
-
-
+	moon.position.set(50, 90, 0);
+	
+	pivot_moon =  new THREE.Object3D();
+	//pivot_moon.position.set(ziemia.position);
+	console.log(pivot_moon.position);
 
 	// vulkan
 
@@ -107,6 +158,9 @@ function init() {
 	scene.add(vulkan);
 	vulkan.position.set(120, 120, 0);
 
+	pivot_vulkan = new THREE.Object3D();
+	pivot_vulkan.position.set(120, 0, 0);
+
 	// donut
 
 	donut = new THREE.Mesh(
@@ -115,26 +169,11 @@ function init() {
 
 	scene.add(donut);
 	donut.position.set(150, 180, 0);
+	pivot_donut = new THREE.Object3D();
+	pivot_donut.position.set(150, 0, 0);
 
-
-	cherry = new THREE.Mesh(
-		new THREE.SphereGeometry(4, 20, 20, 20),
-		new THREE.MeshBasicMaterial({ color: 0xff3030 }));
-
-	cherry.position.set(180, 200, 0);
-
-
-	ancor_point = new THREE.Mesh(
-		new THREE.SphereGeometry(0, 0, 0, 0),
-		new THREE.MeshBasicMaterial({ color: 0xff3030 ,castShadow : true,receiveShadow : true}));
-
-	//ancor_point.normalize();
-	ancor_point.position.set(193,200,0)
-
-	cherry_group = new THREE.Group()
-	cherry_group.add(cherry);
-	cherry_group.add(ancor_point);
-	scene.add(cherry_group);
+	
+	
 
 
 	// weird_thing
@@ -144,8 +183,10 @@ function init() {
 		new THREE.MeshBasicMaterial({ color: 0xff3030 }));
 
 	scene.add(weird_thing);
-	weird_thing.position.set(200, 160, 0);
+	weird_thing.position.set(150, 180, 0);
 
+	pivot_weird_thing = new THREE.Object3D();
+	pivot_weird_thing.position.set(150, 0, 0);
 
 
 	render();
@@ -155,11 +196,52 @@ function init() {
 
 
 
+angle_arrakis = 0;
+angle_ziemia = 0, angle_vulkan = 0, angle_donut = 0, angle_weird_thing = 0;
 
-let x = 0;
 function render() {
-	x+=0.001;
-	cherry_group.rotation+=0.001;
+	
+
+	angle_arrakis += 0.1;
+	Arrakis.position.copy(orbit(pivot_Arrakis, Arrakis, angle_arrakis, 0, 0));
+	Arrakis.rotation.x+=Math.random()/10;
+	Arrakis.rotation.y+=Math.random()/10;
+	Arrakis.rotation.z+=Math.random()/10;
+
+	angle_ziemia += 0.4;
+	ziemia.position.copy(orbit(pivot_ziemia, ziemia, angle_ziemia, 0, 0));
+	
+
+	pivot_moon.position.x = ziemia.position.x;
+	
+	pivot_moon.position.y = ziemia.position.y;
+	
+	pivot_moon.position.z = ziemia.position.z;
+
+
+	moon.position.copy(moonorbit(pivot_moon,angle_weird_thing,8));
+
+
+	angle_vulkan += 1.4;
+	vulkan.position.copy(orbit(pivot_vulkan, vulkan, angle_vulkan, 0, 0));
+	vulkan.rotation.x+=Math.random()/10;
+	vulkan.rotation.y+=Math.random()/10;
+	vulkan.rotation.z+=Math.random()/10;
+
+	angle_donut -= 1.3;
+	donut.position.copy(orbit(pivot_donut, donut, angle_donut, 0, 0));
+	donut.rotation.x+=Math.random()/10;
+	donut.rotation.y+=Math.random()/10;
+	donut.rotation.z+=Math.random()/10;
+
+
+
+	angle_weird_thing += 1.4;
+	weird_thing.position.copy(orbit(pivot_weird_thing, weird_thing, angle_weird_thing, 0, 0));
+	weird_thing.rotation.x+=Math.random()/10;
+	weird_thing.rotation.y+=Math.random()/10;
+	weird_thing.rotation.z+=Math.random()/10;
+
 
 	renderer.render(scene, free_camera);
 
